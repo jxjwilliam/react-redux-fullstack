1159bfeeab03035327f5480ecc430b6f7c4192da
William.Jiang <williamjxj@hotmail.com>
2017-04-04 11:17:30 -0400

    course 4: seperate js to Root, configureStore

---
+++ src/configureStore.js
@@ -0,0 +1,27 @@
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import { loadState, saveState } from './localStorage'
+import throttle from 'lodash/throttle';
+
+const configureStore = () => {
+
+    const persistedState = loadState();
+
+    const store = createStore(
+        todoApp,
+        persistedState
+    );
+
+    store.subscribe(throttle(() => {
+        saveState({todos: store.getState().todos})
+    }), 1000);
+
+    /**
+     * {"todos":[{"id":0,"text":"hi","completed":false},{"id":1,"text":"ho","completed":true}],"visibilityFilter":"SHOW_ALL"}
+     * {"todos":[],"visibilityFilter":"SHOW_ALL"}
+     */
+    console.info(JSON.stringify(store.getState()));
+    return store;
+};
+
+export default configureStore;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
+import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './components/App'
-import todoApp from './reducers'
-import { loadState, saveState } from './localStorage'
-import throttle from 'lodash/throttle';
+import configureStore from './configureStore'
+import Root from './components/Root';
 
-const persistedState = loadState();
-const store = createStore(
-    todoApp,
-    persistedState
-);
-
-store.subscribe(throttle(() => {
-    saveState({todos: store.getState().todos})
-}), 1000);
-
-/**
- * {"todos":[{"id":0,"text":"hi","completed":false},{"id":1,"text":"ho","completed":true}],"visibilityFilter":"SHOW_ALL"}
- * {"todos":[],"visibilityFilter":"SHOW_ALL"}
- */
-console.info(JSON.stringify(store.getState()));
+const store = configureStore()
 
 render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
+    <Root store={store} />,
     document.getElementById('root')
 )
